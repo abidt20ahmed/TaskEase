e687a53bdf5c1ec559e883c1792835002d66dbcb
abidt20ahmed <abidahmedmahin21@gmail.com>
2024-02-07 23:36:06 +0600

    TaskEase Top responsive

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -3,21 +3,23 @@ import { FiPlus } from "react-icons/fi";
 const Tasks = () => {
     return (
         <div className="h-screen px-3 md:px-0 container mx-auto mt-10">
-            <div className="flex justify-between items-center">
+            <div className="flex flex-col sm:flex-row justify-between items-center gap-5">
                 <div>
                     <h1 className="font-bold text-3xl text-blue-500">TaskEase</h1>
                 </div>
-                <div className="flex gap-5">
-                    <button className="border-2 border-secondary/20 rounded-xl h-10 w-10  grid place-content-center text-secondary hover:text-white transition-all">
-                        <p className="font-bold text-gray-600">5</p>
-                    </button>
-                    <button className="border-2 border-secondary/20 rounded-xl h-10 w-10 grid place-content-center text-secondary hover:text-white transition-all">
-                        <p className="font-bold text-gray-600">5</p>
-                    </button>
-                    <button className="border-2 border-secondary/20 rounded-xl h-10 w-10 grid place-content-center text-secondary hover:text-white transition-all">
-                        <p className="font-bold text-gray-600">5</p>
-                    </button>
-                    <button className="btn flex justify-center items-center gap-2"> <FiPlus className="w-5 h-5" /> Add Task</button>
+                <div className="flex flex-col-reverse sm:flex-row justify-center items-center gap-5">
+                    <div className="flex justify-center items-center gap-5">
+                        <button className="border-2 rounded-xl h-10 w-10  grid place-content-center hover:text-white transition-all">
+                            <p className="font-bold text-gray-600">5</p>
+                        </button>
+                        <button className="border-2 rounded-xl h-10 w-10 grid place-content-center hover:text-white transition-all">
+                            <p className="font-bold text-gray-600">5</p>
+                        </button>
+                        <button className="border-2 rounded-xl h-10 w-10 grid place-content-center hover:text-white transition-all">
+                            <p className="font-bold text-gray-600">5</p>
+                        </button>
+                    </div>
+                    <button className="btn flex justify-center items-center gap-2 max-w-28"> <FiPlus className="w-5 h-5" /> Add Task</button>
                 </div>
             </div>
         </div>
