dbee956d6b81abe65bf8a44158ccfc9fc2244957
abidt20ahmed <abidahmedmahin21@gmail.com>
2024-02-09 01:28:53 +0600

    final check

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -2,7 +2,7 @@ import TaskTrack from "../components/TrackTasks";
 
 const Tasks = () => {
     return (
-        <div className="h-screen px-3 lg:px-0 container mx-auto mt-10">
+        <div className="px-3 lg:px-0 container mx-auto mt-10">
             <div className="flex flex-col sm:flex-row justify-between items-center gap-5">
                 <div>
                     <h1 className="font-bold text-3xl text-blue-500">TaskEase</h1>
@@ -12,7 +12,7 @@ const Tasks = () => {
                     <li className="font-semibold list-disc text-green-500" title="Color Indicator">Completed</li>
                 </ul>
             </div>
-            <div className="lg:w-1/3 mx-auto gap-5 mt-10">
+            <div className="lg:w-2/5 mx-auto mt-10">
                 <TaskTrack state={'Pending'} tasks={5} />
             </div>
         </div>
